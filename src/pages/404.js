ca0244f9159ab6da2a9dce6d95d34668ef95a781
scottbailey-sinch <scott.bailey@sinch.com>
2024-01-02 16:48:43 +0000

    Finished Gatsby Tutorial Part 4

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Seo from '../components/seo'
+
 
 const pageStyles = {
   color: "#232129",
@@ -46,4 +48,4 @@ const NotFoundPage = () => {
 
 export default NotFoundPage
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <Seo title="Not found" />
