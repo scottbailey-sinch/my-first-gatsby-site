b530c34ce5a1e88451deb9999a13befb7b1b0977
scottbailey-sinch <scott.bailey@sinch.com>
2024-01-03 13:28:04 +0000

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,32 +4,39 @@ import Layout from '../components/layout'
 import Seo from '../components/seo'
 
 
+// imports
+
 const BlogPage = ({ data }) => {
     return (
       <Layout pageTitle="My Blog Posts">
-        <ul>
         {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>
-              {node.name}
-            </li>
+          data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
+            </article>
           ))
         }
-        </ul>
       </Layout>
     )
   }
-
-export const query = graphql`
-  query {
-      allFile {
+  
+  export const query = graphql`
+    query {
+      allMdx(sort: { frontmatter: { date: DESC }}) {
         nodes {
-          name
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+          }
+          id
+          excerpt
         }
       }
     }
   `
-
-export const Head = () => <Seo title="My Blog Posts" />
-
-export default BlogPage
+  
+  export const Head = () => <Seo title="My Blog Posts" />
+  
+  export default BlogPage
